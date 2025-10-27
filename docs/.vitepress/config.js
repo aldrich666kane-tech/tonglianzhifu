60143c75f4684954995e09015d39f04abc36866a
aldrich666kane-tech <aldrich666kane-tech@users.noreply.github.com>
2025-10-27 22:02:50 +0800

    Initial commit

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,24 @@
+export default {
+  title: '支付知识体系',
+  description: '一个关于支付体系的知识库',
+  themeConfig: {
+    nav: [
+      { text: '首页', link: '/' },
+    ],
+    sidebar: [
+      {
+        text: '知识体系',
+        items: [
+          { text: '卷一', link: '/volume1' },
+          { text: '卷二', link: '/volume2' },
+          { text: '卷三', link: '/volume3' },
+          { text: '卷四', link: '/volume4' },
+          { text: '卷五', link: '/volume5' },
+          { text: '卷六', link: '/volume6' },
+          { text: '卷七', link: '/volume7' },
+          { text: '卷八', link: '/volume8' },
+        ]
+      }
+    ]
+  }
+}
